cb691f6b1e41cd7d0ef13ed24a6075af369ea6bd
Nicolás Javier Irazabal <nirazabal@despegar.com>
2019-07-05 19:24:35 -0300

    archivos

---
+++ src/views/lib/code-to-blocks.js
@@ -0,0 +1,39 @@
+function _separateFunctions(){
+    //contar pares de llaves, si son dos, es una sola declaracion, retorna lo mismo
+    // func1(){  func1a(){}      func1b(){}            }
+}
+/* 
+
+//transformar este código java
+
+  public void run(){
+    setColors('#ff0000','#ff6600','#3333ff');
+   };
+  
+// en este xml
+
+<xml xmlns="http://www.w3.org/1999/xhtml" id="startBlocks" style="display:none">
+        <block type="fn_run" id="z}S%An_MLFM!`aVm[=:%" x="50" y="50" movable="false">
+            <statement name="run">
+                <block type="fn_setcolors" id="Cfxn*MhWGPA{4=$DqZ54">
+                    <value name="bodyColor">
+                        <block type="colour_picker" id="!+puTde0`U:[`%2G(WgT">
+                            <field name="COLOUR">#ff0000</field>
+                        </block>
+                    </value>
+                    <value name="gunColor">
+                        <block type="colour_picker" id="9]NLUuf^UXr6iA4NhRu2">
+                            <field name="COLOUR">#ff6600</field>
+                        </block>
+                    </value>
+                    <value name="radarColor">
+                        <block type="colour_picker" id="KGZ~AT5p-E4Q}HmlsRSV">
+                            <field name="COLOUR">#3333ff</field>
+                        </block>
+                    </value>
+                </block>
+            </statement>
+        </block>
+    </xml>
+
+*/
